97e64b2522d685d9d7799d33499ab86e4184ae1c
mahtabmardani88 <mahtab.mardani88@gmail.com>
2024-09-02 17:20:28 +0200

    update all propTypes

--- week1/project/ecommerce/src/CategoryList.jsx
+++ week1/project/ecommerce/src/CategoryList.jsx
@@ -1,28 +1,27 @@
-
-import PropTypes from 'prop-types';
+/* eslint-disable react/prop-types */
+import React  from 'react';
 import categories from './all-categories.js';
 
-const CategoryList = ({ selectedCategory, onSelectCategory }) => { 
-    return (
-        <div>
-            {categories.map((category) => (
-                <button
-                    key={category.id} 
-                    onClick={() => onSelectCategory(category.name)}
-                    style={{
-                        fontWeight: category.name === selectedCategory ? 'bold' : 'normal',
-                    }}
-                >
-                    {category.name}
-                </button>
-            ))}
-        </div>
-    );
-};
 
-CategoryList.propTypes = {
-    selectedCategory: PropTypes.string.isRequired,
-    onSelectCategory: PropTypes.func.isRequired,
+const CategoryList = ({ selectedCategory, onSelectCategory }) => {
+  return (
+    <div>
+      {categories.map((categoryName, index) => {
+        const cleanedCategoryName = categoryName.replace('FAKE: ', '').trim();
+        return (
+          <button
+            key={index}
+            onClick={() => onSelectCategory(cleanedCategoryName)}
+            style={{
+              fontWeight: cleanedCategoryName === selectedCategory ? 'bold' : 'normal',
+            }}
+          >
+            {cleanedCategoryName}
+          </button>
+        );
+      })}
+    </div>
+  );
 };
 
 export default CategoryList;
--- week1/project/ecommerce/src/ProductList.jsx
+++ week1/project/ecommerce/src/ProductList.jsx
@@ -1,25 +1,26 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
 import products from './all-products.js';
-import PropTypes from 'prop-types';
 import './ProductList.css';
 
-const ProductList = ({ selectedCategory }) => {
-    const filterProduct = selectedCategory ? products.filter((product) => product.category === selectedCategory) : products;
-    return (
-        <div className="product-list">
-            {filterProduct.map((product) => (
-                <div key={product.id} className="product-card">
-                     <img src={product.image} alt={product.name} className="product-image" /> 
-                    <h2>{product.name}</h2>
-                    <p>{product.description}</p>
-                    <p>Price : ${product.price}</p>
-                </div>
-            ))}
-        </div>
-    );
-};
 
-ProductList.propTypes = {
-    selectedCategory: PropTypes.string,
+const ProductList = ({ selectedCategory }) => {
+  const filteredProducts = selectedCategory
+    ? products.filter((product) => product.category === selectedCategory)
+    : products;
+
+  return (
+    <div className="product-list">
+      {filteredProducts.map((product) => (
+        <div key={product.id} className="product-card">
+          <img src={product.image} alt={product.title} className="product-image" />
+          <h2>{product.title}</h2>
+          <p>{product.description}</p>
+          <p>Price: ${product.price}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ProductList;
