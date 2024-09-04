4a121a99348bc107c6a8f4095066171a684789db
Moayad Mohammed <moayad.ashabi@gmail.com>
2024-09-05 01:54:49 +0200

    removed comments

--- week2/project/ecommerce/src/App.jsx
+++ week2/project/ecommerce/src/App.jsx
@@ -4,16 +4,6 @@ import CategoryController from './components/CategoryController.jsx'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Product from './components/product.jsx'
 
-
-// Your app needs to go to a detail page /product/:id whenever you click on the product card
-//  in the list. This should get the details from the endpoint: 
-//  https://fakestoreapi.com/products/<id>. For now we won't add a navigation bar,
-//   the browsers 'back' button will do the trick. 
-//   TIP: You will need to add the react-router-dom package and add the routing to your app 
-//   regardless.
-// You need to deploy your app somewhere (using something like netlify) and put the link in your PR!
-//  Make it a different one than the previous week.
-
 function App() {
   const [selectedCategory, setSelectedCategory] = useState(null)
 
@@ -32,16 +22,3 @@ function App() {
 }
 
 export default App
-
-{/* <>
-<div>
-  <h1>PRODUCTS</h1>
-</div>
-<div className='app'>
-  <CategoryController
-    selectedCategory={selectedCategory}
-    handleCategoryClick={handleCategoryClick}
-  />
-</div>
-</> */}
-
