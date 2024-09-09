47c117ec5a48c28aba6f6634a6e0a654d2c0d1d2
mustafa durmus <mdurmusnl@gmail.com>
2024-09-09 13:41:13 +0200

    displaying ten users

--- week2/prep-exercises/1-generate-random-person-details/assignment/src/components/PersonController.jsx
+++ week2/prep-exercises/1-generate-random-person-details/assignment/src/components/PersonController.jsx
@@ -1,32 +1,41 @@
-// src/components/PersonController.jsx
 import { useState, useEffect } from "react";
 import Person from "./Person";
-const url = "https://www.randomuser.me/api?results=1";
+
+const url = "https://www.randomuser.me/api?results=10"; // Fetch 10 random users
 
 function PersonController() {
-  const [person, setPerson] = useState(null);
+  const [people, setPeople] = useState([]); // Change state to hold an array of people
 
-  const getPerson = async () => {
+  // Fetch 10 random people from the API
+  const getPeople = async () => {
     const response = await fetch(url);
     const data = await response.json();
-    const personData = destructurePerson(data);
-    setPerson(personData);
-  };
-  const destructurePerson = (data) => {
-    // Extract first name, last name, and email from API response
-    const personData = {
-      firstName: data.results[0].name.first,
-      lastName: data.results[0].name.last,
-      email: data.results[0].email,
-    };
-    return personData;
+    const peopleData = destructurePeople(data);
+    setPeople(peopleData);
   };
 
+  // Destructure the API response to extract necessary information for each person
+  const destructurePeople = (data) => {
+    // Map through the results to create an array of person objects
+    return data.results.map((person) => ({
+      firstName: person.name.first,
+      lastName: person.name.last,
+      email: person.email,
+    }));
+  };
+
+  // Fetch people when the component mounts
   useEffect(() => {
-    getPerson();
+    getPeople();
   }, []);
 
-  return <Person person={person} />;
+  return (
+    <div>
+      {people.map((person, index) => (
+        <Person key={index} person={person} /> // Render a Person component for each person
+      ))}
+    </div>
+  );
 }
 
 export default PersonController;
